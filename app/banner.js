0509bb8cda7b612f2ae3b435810ccd20f95ffaca
Richard McSharry <rmcsharry@gmail.com>
2024-04-02 11:44:47 +0200

    add button handlers

--- app/banner.js
+++ app/banner.js
@@ -1,6 +1,12 @@
-// app/banner.js
 'use client';
 import { useEffect, useState } from "react";
+
+export function cookieConsentGiven() {
+  if (!localStorage.getItem('cookie_consent')) {
+    return 'undecided';
+  }
+  return localStorage.getItem('cookie_consent');
+}
 
 export default function Banner() {
   const [consentGiven, setConsentGiven] = useState('');
@@ -8,8 +14,18 @@ export default function Banner() {
   useEffect(() => {
     // We want this to only run once the client loads
     // or else it causes a hydration error
-    setConsentGiven('undecided');
+    setConsentGiven(cookieConsentGiven());
   }, []);
+
+  const handleAcceptCookies = () => {
+    localStorage.setItem('cookie_consent', 'yes');
+    setConsentGiven('yes');
+  };
+
+  const handleDeclineCookies = () => {
+    localStorage.setItem('cookie_consent', 'no');
+    setConsentGiven('no');
+  };
 
   return (
     <div>
@@ -20,11 +36,11 @@ export default function Banner() {
             the product and help us improve it.
             Please accept cookies to help us improve.
           </p>
-          <button type="button">Accept cookies</button>
+          <button type="button" className="border border-black border-spacing-2 p-1" onClick={handleAcceptCookies}>Accept cookies</button>
           <span> </span>
-          <button type="button">Decline cookies</button>
+          <button type="button" className="border border-black border-spacing-2 p-1" onClick={handleDeclineCookies}>Decline cookies</button>
         </div>
       )}
     </div>
-  )
+  );
 }
